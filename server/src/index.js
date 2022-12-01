0a9bf9fdc5c53cbeed60b3102f2fe69c5cdc48dc
Patrick Lai <patrick.lai@ringcentral.com>
2022-12-01 10:45:08 +0800

    add feature

--- server/src/index.js
+++ server/src/index.js
@@ -3,37 +3,22 @@ const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
-const mocks = {
-    Query: () => ({
-        tracksForHome: () => [...new Array(6)],
-    }),
-    Track: () => ({
-        id: () => 'track_01',
-        title: () => 'Astro Kitty, Space Explorer',
-        author: () => ({
-            name: 'Grumpy Cat',
-            photo: 'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
-        }),
-        thumbnail: () => 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
-        length: () => 1210,
-        modulesCount: () => 6,
-    })
-};
+async function startApolloServer() {
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => {
-        return {
-            trackAPI: new TrackAPI(),
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        dataSources: () => {
+            return {
+                trackAPI: new TrackAPI(),
+            }
         }
-    }
-});
+    });
+    const {url} = await server.listen({
+        port: process.env.PORT || 4000
+    });
 
-server.listen().then(() => {
-    console.log(`
-        🚀  Server is running!
-        🔉  Listening on port 4000
-        📭  Query at http://localhost:4000
-    `);
-});
+    console.log(`🚀  Server ready at ${url}`);
+}
+
+startApolloServer();
